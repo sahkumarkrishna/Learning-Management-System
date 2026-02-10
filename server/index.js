61a757415f84f87f7050be833a138dcdb91298d9
Krishna Kumar <159722967+sahkumarkrishna@users.noreply.github.com>
2026-02-10 21:40:45 +0530

    Update Node.js version to 20.x for Vercel

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ import mediaRoute from "./routes/media.router.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 console.log("Loaded SECRET_KEY:", `"${process.env.SECRET_KEY}"`); // Debug line
 connectDB();
@@ -25,6 +28,10 @@ app.use(
   })
 );
 
+// Fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
@@ -38,6 +45,13 @@ app.get("/home", (_, res) => {
     message: "Hello, I am coming from the backend",
   });
 });
+// Serve frontend (after API routes)
+app.use(express.static(path.join(__dirname, "../client/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/client/dist/index.html"));
+});
+
 
 // Start the server
 app.listen(PORT, () => {
